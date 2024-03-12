ec8410935ffc7c66c371b928bbaa45abc5bb8730
Tambatoe <salvatore.merol@gmail.com>
2024-03-12 21:54:42 +0100

    classification

--- network_classification.py
+++ network_classification.py
@@ -38,7 +38,8 @@ if __name__ == '__main__':
     input_shape = (34, 1)
 
     model = tf.keras.Sequential([
-        # tf.keras.layers.Conv1D(filters=64, kernel_size=2, activation='relu', input_shape=input_shape),
+        # tf.keras.layers.Conv1D(filters=64, kernel_size=2, strides=2, activation='relu', input_shape=input_shape),
+        # tf.keras.layers.Flatten(input_shape=(17, 1)),
         tf.keras.layers.Flatten(input_shape=input_shape),
         tf.keras.layers.Dropout(rate=0.2),
         tf.keras.layers.Dense(128, activation='relu'),
@@ -47,13 +48,13 @@ if __name__ == '__main__':
         tf.keras.layers.Dense(1, activation='sigmoid')
     ])
 
-    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.003),
+    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.0003),
                   loss=tf.keras.losses.BinaryCrossentropy(),
                   metrics=['accuracy'])
 
     model.fit(train_ds,
               validation_data=test_ds,
-              epochs=100)
+              epochs=5000)
 
     evaluation = model.evaluate(val_ds)
     print(evaluation)
